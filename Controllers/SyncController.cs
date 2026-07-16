2f30e2f47ac57bf14352c1bd9d358b59c0a69ff9
S-bon-debug <sonngocson25@gmail.com>
2026-07-16 22:47:36 +0700

    fix: resolve ObjectDisposedException in SyncController by using IServiceScopeFactory

--- Controllers/SyncController.cs
+++ Controllers/SyncController.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using PaperService.Services;
+using Microsoft.Extensions.DependencyInjection;
 using System.Threading.Tasks;
 using System.Threading;
 
@@ -9,25 +10,25 @@ namespace PaperService.Controllers
     [Route("api/papers/[controller]")]
     public class SyncController : ControllerBase
     {
-        private readonly ISyncJobService _syncJobService;
+        private readonly Microsoft.Extensions.DependencyInjection.IServiceScopeFactory _scopeFactory;
 
-        public SyncController(ISyncJobService syncJobService)
+        public SyncController(Microsoft.Extensions.DependencyInjection.IServiceScopeFactory scopeFactory)
         {
-            _syncJobService = syncJobService;
+            _scopeFactory = scopeFactory;
         }
 
         [HttpPost("trigger")]
         public async Task<IActionResult> TriggerSync(CancellationToken cancellationToken)
         {
             // Run async job in background, do not await it so API returns immediately
-            _ = Task.Run(() => _syncJobService.DoSyncWorkAsync(CancellationToken.None));
+            _ = Task.Run(async () => { using var scope = _scopeFactory.CreateScope(); var syncJobService = scope.ServiceProvider.GetRequiredService<ISyncJobService>(); await syncJobService.DoSyncWorkAsync(CancellationToken.None); });
             return Accepted(new { message = "Sync job has been triggered and is running in the background." });
         }
 
         [HttpDelete("wipe")]
         public async Task<IActionResult> WipeMockData(CancellationToken cancellationToken)
         {
-            await _syncJobService.WipeMockDataAsync(cancellationToken);
+            using var scope = _scopeFactory.CreateScope(); var syncJobService = scope.ServiceProvider.GetRequiredService<ISyncJobService>(); await syncJobService.WipeMockDataAsync(cancellationToken);
             return Ok(new { message = "All mock data wiped successfully. Sync cursors have been reset." });
         }
     }
